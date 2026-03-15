80ec65763e8cb3a72fc4e5052ec76e302062f0e8
WAEL SK <47039100+waelsk95l@users.noreply.github.com>
2026-03-15 13:47:15 +0300

    Update alert_generator.py

--- Phase2/Automation/alert_generator.py
+++ Phase2/Automation/alert_generator.py
@@ -1,1 +1,9 @@
+from log_parser import load_logs
+from detection_engine import detect_lsass_access
 
+log = load_logs("../Case9_LSASS_Access_Detection/sample-log.json")
+
+if detect_lsass_access(log):
+    print("[ALERT] Suspicious LSASS access detected!")
+else:
+    print("[OK] No suspicious activity.")
