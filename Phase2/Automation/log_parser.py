107136c3e14e45cfbaeaa42e8d763366ecabe9a1
WAEL SK <47039100+waelsk95l@users.noreply.github.com>
2026-03-19 23:29:48 +0300

    Update log_parser.py

--- Phase2/Automation/log_parser.py
+++ Phase2/Automation/log_parser.py
@@ -1,5 +1,5 @@
 import json
 
-def load_logs(file_path):
-    with open(file_path, 'r') as f:
+def load_json_file(file_path: str):
+    with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
