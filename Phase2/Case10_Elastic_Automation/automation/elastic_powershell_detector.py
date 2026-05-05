450d48fd952d811d29c9e90f49df39f4ced83bd5
WAEL SK <47039100+waelsk95l@users.noreply.github.com>
2026-05-05 11:46:47 +0300

    Update elastic_powershell_detector.py

--- Phase2/Case10_Elastic_Automation/automation/elastic_powershell_detector.py
+++ Phase2/Case10_Elastic_Automation/automation/elastic_powershell_detector.py
@@ -1,1 +1,20 @@
 
+from elasticsearch import Elasticsearch
+import urllib3
+
+urllib3.disable_warnings()
+
+ES_URL = "https://localhost:9200"
+ES_USER = "elastic"
+ES_PASS = "elastic"
+
+es = Elasticsearch(
+    ES_URL,
+    basic_auth=(ES_USER, ES_PASS),
+    verify_certs=False
+)
+
+if es.ping():
+    print("[OK] Connected to Elasticsearch")
+else:
+    print("[ERROR] Connection failed")
